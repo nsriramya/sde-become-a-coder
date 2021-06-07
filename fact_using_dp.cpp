4bb7734917ad562908c251f0d296efa22e77afb5
nsriramya <71599848+nsriramya@users.noreply.github.com>
2021-06-07 18:25:07 +0530

    Add files via upload

---
+++ fact_using_dp.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int dp[1000000];
+int factorial(int n)
+{
+	if(n==1)
+	{
+		return 1;
+	}
+	if(dp[n]!=-1)
+	{
+		return dp[n];
+	}
+	return dp[n]=n*factorial(n-1);
+}
+int main()
+{
+	int num;
+	cin>>num;
+	for(int i=0;i<num+1;i++)
+	{
+		dp[i]=-1;
+	}
+	cout<<factorial(num)<<endl;
+	for(int i=0;i<num+1;i++)
+	{
+		cout<<dp[i]<<" ";
+	}
+	return 0;
+}
+	
