50ddb58b3ca71b39b7975a1d52e1ebaa5a74f2c2
nsriramya <71599848+nsriramya@users.noreply.github.com>
2021-10-07 20:21:26 +0530

    Add files via upload

---
+++ camelcase.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'camelcase' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int camelcase(string s) {
+int c=0;
+for(int i=0;i<s.length();i++)
+{
+if((s[i]>='A')&&(s[i]<='Z'))
+{
+    c++;
+}
+}
+return c+1;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    int result = camelcase(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
